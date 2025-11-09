1dfc27bb52cfff6fd1300f8345d2b516fc9a1008
unknown <eloysalazar2100@gmail.com>
2025-11-08 21:22:13 -0400

    Latienda_1.0.2.5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-    apiUrl: 'http://localhost:8080'
-
- // apiUrl: 'https://ko4wgwo0c8gkkkw888808okk.systemash.com'
+   // apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://ko4wgwo0c8gkkkw888808okk.systemash.com'
 
 };
