a334a8dc7f3c1ce47693241852384ec1e5de1aaf
unknown <eloysalazar2100@gmail.com>
2026-01-02 16:31:11 -0400

    1.0.0.1z

--- src/environments/environmen.ts
+++ src/environments/environmen.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-   // apiUrl: 'http://localhost:8080' // O la URL de tu backend local
+  //  apiUrl: 'http://localhost:8080' // O la URL de tu backend local
    apiUrl: 'https://ko4wgwo0c8gkkkw888808okk.systemash.com'
 };
